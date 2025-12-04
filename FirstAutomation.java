cd57d07ef2099cb45201c2829b2b08fd68e0dd00
Praveen kumar.P <battlegroundmobile2006@gmail.com>
2025-12-04 13:00:33 +0530

    Add files via upload
    FirstAutomation

---
+++ FirstAutomation.java
@@ -0,0 +1,12 @@
+package DAY1;
+
+public class FirstAutomation {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ 
